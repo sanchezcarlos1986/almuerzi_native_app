21bea3ab9a018d7b4ebe352624a0870545be65a9
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2020-12-31 18:03:59 -0300

    feat(constants): using https instead http to fix cors issue

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,9 @@
 export const API_URL = {
-  MEALS: 'http://serverless-rust-ten.vercel.app/api/meals',
-  ORDERS: 'http://serverless-rust-ten.vercel.app/api/orders',
+  MEALS: 'https://serverless-rust-ten.vercel.app/api/meals',
+  ORDERS: 'https://serverless-rust-ten.vercel.app/api/orders',
 };
+
+// export const API_URL = {
+//   MEALS: 'http://localhost:3000/api/meals',
+//   ORDERS: 'http://localhost:3000/api/orders',
+// };
