f2317aede863ab2ee600e6e8972da394c337fa4e
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2020-12-31 18:04:35 -0300

    feat(screens): created postOrder method in Modal

--- src/screens/Modal.js
+++ src/screens/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {View, Text, StyleSheet, Button} from 'react-native';
 import {useFetch} from '~/hooks';
 import {API_URL} from '~/constants';
@@ -7,6 +8,32 @@ import {Loader} from '~/components';
 const Modal = ({navigation}) => {
   const id = navigation.getParam('_id');
   const {loading, data} = useFetch(`${API_URL.MEALS}/${id}`);
+
+  const postOrder = async () => {
+    const token = await AsyncStorage.getItem('token');
+
+    if (token) {
+      fetch(API_URL.ORDERS, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: token,
+        },
+        body: JSON.stringify({
+          meal_id: id,
+        }),
+      })
+        .then(res => {
+          if (res.status !== 201) {
+            return alert('La orden no pudo ser generada');
+          }
+
+          alert('Ordern fue generada con exito');
+          navigation.navigate('Meals');
+        })
+        .catch(err => console.log(err));
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -17,26 +44,7 @@ const Modal = ({navigation}) => {
           <Text>{data._id}</Text>
           <Text>{data.name}</Text>
           <Text>{data.desc}</Text>
-          <Button
-            title="Accept"
-            onPress={() =>
-              fetch(`${API_URL.ORDERS}`, {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  meal_id: id,
-                  user_id: 'bleble',
-                }),
-              })
-                .then(() => {
-                  alert('Ordern fue generada con exito');
-                  navigation.navigate('Meals');
-                })
-                .catch(err => console.log(err))
-            }
-          />
+          <Button title="Accept" onPress={postOrder} />
           <Button title="Cancel" onPress={() => navigation.navigate('Meals')} />
         </View>
       )}
